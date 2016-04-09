86c7f5e3cb8e653fe02a528d1906a2c3852fae4a
David Arias <davidjosepharias@gmail.com>
2016-04-08 18:38:07 -0700

    intial commit

---
+++ src/app/index.module.js
@@ -0,0 +1,10 @@
+import { config } from './index.config';
+import { routerConfig } from './index.route';
+import { runBlock } from './index.run';
+import { MainController } from './main/main.controller';
+
+angular.module('appFdr', ['ngCookies', 'ngSanitize', 'ngRoute'])
+  .config(config)
+  .config(routerConfig)
+  .run(runBlock)
+  .controller('MainController', MainController);
---
+++ src/app/main/main.controller.spec.js
@@ -0,0 +1,13 @@
+describe('controllers', () => {
+  let vm;
+
+  beforeEach(angular.mock.module('appFdr'));
+
+  beforeEach(inject(($controller) => {
+    vm = $controller('MainController');
+  }));
+
+  it('foo', () => {
+    
+  });
+});
