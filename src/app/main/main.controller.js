55f6a750fd1c4209ef2b31ac5cb67f665b7632ca
David Arias <davidjosepharias@gmail.com>
2016-04-11 00:08:08 -0700

    sidebar refactor

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -7,6 +7,28 @@ class MainController {
     this.productsService = productsService;
     this.products        = null;
 
+    this.filterProducts = {
+      'guitars': true,
+      'american_standard': true,
+      'guitar_bass_parts': true,
+      'knobs_kits_pickup_covers': true,
+      'classic': true,
+      'pickguards_backplates': true,
+      'vintage_modified_models': true,
+      'miscellaneous_parts': true
+    };
+
+    this.productFilterNames = [
+      { label: 'Guitars', id: 'guitars' },
+      { label: 'American Standard', id: 'american_standard' },
+      { label: 'Guitar and Bass Parts', id: 'guitar_bass_parts' },
+      { label: 'Knobs Kits Pickup Covers', id: 'knobs_kits_pickup_covers' },
+      { label: 'Classic', id: 'classic' },
+      { label: 'Pickguards Backplates', id: 'pickguards_backplates' },
+      { label: 'Vintage Modified Models', id: 'vintage_modified_models' },
+      { label: 'Miscellaneous Parts', id: 'miscellaneous_parts' }
+    ];
+
     this.init();
   }
 
